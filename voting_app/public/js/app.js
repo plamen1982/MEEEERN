5998f6ed2db097522382413b4c5cb2356b5ff254
plamen1982 <plamen.hristov@ymail.com>
2017-03-21 22:21:31 -0400

    Added sorted functionality for votes

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -1,7 +1,10 @@
 
 class ProductList extends React.Component {
   render() {
-    const productComponents = Seed.products.map((product) => (
+    const products = Seed.products.sort((a, b) => {
+        return b.votes - a.votes
+    })  
+    const productComponents = products.map((product) => (
       <Product
         key={'product-' + product.id}
         id={product.id}
@@ -28,6 +31,12 @@ class Product extends React.Component {
                     <img src = {this.props.productImageUrl}/>
                 </div>
                 <div className = "middle aligned content">
+                    <div className ="header">
+                        <a>
+                            <i className = "large caret up icon"/>
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className = "description">
                         <a href={this.props.url}>
                             {this.props.title}
